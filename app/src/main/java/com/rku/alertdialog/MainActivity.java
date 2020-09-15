211806a717802a444f3c48df113dbe94c20010cb
Parth <pgalani193@rku.ac.in>
2020-09-15 20:55:20 +0530

    Create alert dialog box in android studio...

---
+++ app/src/main/java/com/rku/alertdialog/MainActivity.java
@@ -0,0 +1,56 @@
+package com.rku.alertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogout;
+    AlertDialog.Builder builder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        builder = new AlertDialog.Builder(MainActivity.this);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setTitle("Confirm")
+                        .setCancelable(false)
+                        .setMessage("Do you Really want to Logout")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Clicked Yes Button", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Toast.makeText(MainActivity.this, "Clicked No Button", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Toast.makeText(MainActivity.this, "Clicked Cancel Button", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+}
